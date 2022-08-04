b9fbff98971a783abd94878acf0d9d334a46b795
arisa-t <garisa0328@gmail.com>
2022-08-04 16:24:41 +0900

    change bootloader

--- MikanLoaderPkg/Main.c
+++ MikanLoaderPkg/Main.c
@@ -2,6 +2,7 @@
 #include <Library/UefiLib.h>
 #include <Library/UefiBootServicesTableLib.h>
 #include  <Library/PrintLib.h>
+#include <Library/MemoryAllocationLib.h>
 #include  <Protocol/LoadedImage.h>
 #include  <Protocol/SimpleFileSystem.h>
 #include  <Protocol/DiskIo2.h>
