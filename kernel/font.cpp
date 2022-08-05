4fe9072b512cd5ed6e63faa2772474ca8b60dd92
arisa-t <garisa0328@gmail.com>
2022-08-05 23:04:30 +0900

    change bootloader

--- kernel/font.cpp
+++ kernel/font.cpp
@@ -21,7 +21,7 @@ void WriteAscii(PixelWriter& writer, int x, int y, char c, const PixelColor& col
     }
     for (int dy = 0; dy < 16; ++dy){
         for (int dx = 0; dx < 8; ++dx){
-            if ((kFontA[dy] << dx) & 0x80u) {
+            if ((font[dy] << dx) & 0x80u) {
                 writer.Write(x + dx, y + dy, color);
             }
         }
